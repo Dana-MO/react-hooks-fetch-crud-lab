f47d56b6496e116ff5cf7b7ebc31b1bcd1bc6c39
danaomar <danaomaark@gmail.com>
2023-11-01 18:24:27 +0300

    Completed assignment

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [quiz, setQuiz] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then(r => r.json())
+      .then(data => setQuiz(data))
+  }, []);
+
+  function handleDeleteQuestion(deletedQuesiton) {
+    const upDatedQuiz = quiz.filter((quesiton) => quesiton.id !== deletedQuesiton.id);
+    setQuiz(upDatedQuiz);
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {quiz.map((questions) => (
+          <QuestionItem question={questions} key={questions.id} onDeleteQuestion={handleDeleteQuestion}/>
+        ))}
+      </ul>
     </section>
   );
 }
